d5d95c305ab2a88f6b17d362790498449e092987
Étienne Loiselle <loiselle.etienne@gmail.com>
2015-09-02 09:55:23 -0400

    Plus modulaire
    CSS et JS Externe

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dis moi où</title>
+    <!-- Sets initial viewport load and disables zooming  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <!-- Makes your prototype chrome-less once bookmarked to your phone's home screen -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="white">
+    
+    <link href="css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="leaflet.css" />
+
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/L.Control.Locate.css" />
+<!--[if lt IE 9]>
+    <link rel="stylesheet" href="//rawgithub.com/domoritz/leaflet-locatecontrol/gh-pages/dist/L.Control.Locate.ie.min.css"/>
+    <![endif]-->
+
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js"></script>
+    <script src="js/L.Control.Locate.min.js"></script>
+
+    <!--[if lte IE 8]><link rel="stylesheet" href="libs/leaflet.ie.css" /><![endif]-->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Notre feuille de styles -->
+    <link rel="stylesheet" href="css/styles.css">
+
+    
+</head>
+<body>
+
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xs-12">
+        <div id="map"></div>
+
+        <!-- Contrôle pour changer d'étage -->
+        <div id="controlleEtage">
+            <ul>
+                <li><a href="javascript:changerEtage(5)">5</a></li>
+                <li><a href="javascript:changerEtage(4)">4</a></li>
+                <li><a href="javascript:changerEtage(3)">3</a></li>
+                <li><a href="javascript:changerEtage(2)">2</a></li>
+                <li><a href="javascript:changerEtage(1)">1</a></li>
+            </ul>
+        </div> <!-- #controlleEtage -->
+
+    </div>
+</div>
+
+
+<!-- Fenêtre modal pour rechercher un local, contient le champ de recherche et la longue liste de tout les locaux. Affiché via un bouton dans le menu de navigation et la fonction  $("#modalRechercher").modal('show') dans le fichier app.js-->
+<div class="modal fade" id="modalRechercher" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title text-center" id="myModalLabel">Rechercher</h4>
+    </div>
+    <div class="modal-body">
+        <div id="listeLocaux">
+
+            <!-- Le champ de recherche, filtre dynamiquement la liste via le plugin jQuery LiveFilter-->
+            <input id="inputRechercherModal" class="search form-control" placeholder="Local 2106, Cafétéria, Porte principale" autofocus tabindex="1" />
+
+            <!-- La longue liste qui liste tout les locaux du cégep -->
+            <ul id="listeRechercheLocal">
+            
+                <?php
+                    include "listelocaux.html"
+                ?>
+                
+            </ul>
+        </div> <!--------  listeLocaux  -------->
+    </div> <!--------  .modal-body  -------->
+</div>
+</div>
+</div> <!--------  .modal  -------->
+
+
+</div> <!--------  .container-fluid  -------->
+
+
+<!-- La barre de navigation dans le bas de l'écran, contient trois boutons -->
+<footer class="footer">
+      <div class="container">
+        <ul id="navigationFooter">
+
+            <!-- Apelle la fonction lc.start().....Geolocalisation de l'utilisateur -->
+            <li>
+                <a href="javascript:lc.start()"><i class="material-icons">location_searching</i></a>
+            </li>
+
+            <!-- Ouvre la fenêtre modale de recherche de locaux -->
+            <li>
+                <a id="boutonRechercher" href="javascript:"><i class="material-icons">search</i></a>
+            </li>
+
+            <!-- Ouvre la fenetre d'information sur le projet -->
+            <li>
+                <a href="javascript:"><i class="material-icons">info_outline</i></a>
+            </li>
+        </ul>
+      </div>
+    </footer>
+
+
+
+
+
+
+
+<!-- Tous les scripts javascript nécéssaire pour notre application-->
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<!-- Pour faire bondir les petit markeur lors de leur ajout sur la carte -->
+<script src="js/bouncemarker.js"></script>
+
+<!-- Permet de filtrer dynamiquement la liste des locaux -->
+<script src="js/jquery.liveFilter.js"></script>
+
+<!---------------------------------------------------------------------->
+<!------------------ Les scripts de notre application ------------------>
+<!---------------------------------------------------------------------->
+<script src="js/app.js"></script>
+
+
+</script>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,252 @@
+        // Créé la carte avec les données de Open Street Map
+        var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        osm = new L.TileLayer(osmUrl, {
+            maxZoom: 22,
+            reuseTiles: true, 
+        });
+
+        // Limite de la carte
+        var southWest = L.latLng(46.828064, -71.230556),
+            northEast = L.latLng(46.832086, -71.224612),
+            bounds = L.latLngBounds(southWest, northEast);
+
+        // Affiche la carte
+        var map = new L.Map('map', {
+            layers: [osm],
+            center: new L.LatLng(46.8302871, -71.227337),
+            zoom: 19,
+            //maxBounds: bounds,
+            zoomControl:false
+        });
+            
+
+            // Initialisation de quelque variable, pour régler certain problème en rapport avec leur scope
+            var imageUrl = "";
+            var imageBounds = "";
+            var planEtage = "";
+            var lc = "";
+            var marqueurs = "";
+
+
+            // Icone pour les marqueurs des portes, escaliers, ascenseurs, etc.
+
+            // Icônes pour les portes
+            var iconePortes = L.icon({
+                                iconUrl: 'images/markers/portes.png',
+                                iconSize:     [32, 29], // size of the icon
+                                iconAnchor:   [16, 15], // point of the icon which will correspond to marker's location
+                                popupAnchor:  [2, -13] // point from which the popup should open relative to the iconAnchor
+                                });
+
+            // Icône pour les escalier
+            var iconeEscalier = L.icon({
+                                iconUrl: 'images/markers/escaliers.png',
+                                iconSize:     [32, 32], // size of the icon
+                                iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
+                                popupAnchor:  [2, -13] // point from which the popup should open relative to the iconAnchor
+                                });
+
+
+            
+
+
+                // Geolocalisation de l'utilisateur
+                lc = L.control.locate({
+                                    position: 'topleft',  // set the location of the control
+                                    drawCircle: false,  // controls whether a circle is drawn that shows the uncertainty about the location
+                                    follow: false,  // follow the user's location
+                                    setView: false, // automatically sets the map view to the user's location, enabled if `follow` is true
+                                    keepCurrentZoomLevel: true, // keep the current map zoom level when displaying the user's location. (if `false`, use maxZoom)
+                                    stopFollowingOnDrag: false, // stop following when the map is dragged if `follow` is true (deprecated, see below)
+                                    remainActive: false, // if true locate control remains active on click even if the user's location is in view.
+                                    markerClass: L.circleMarker, // L.circleMarker or L.marker
+                                    circleStyle: {},  // change the style of the circle around the user's location
+                                    markerStyle: {},
+                                    followCircleStyle: {},  // set difference for the style of the circle around the user's location while following
+                                    followMarkerStyle: {},
+                                    icon: 'fa fa-map-marker',  // class for icon, fa-location-arrow or fa-map-marker
+                                    iconLoading: 'fa fa-spinner fa-spin',  // class for loading icon
+                                    circlePadding: [0, 0], // padding around accuracy circle, value is passed to setBounds
+                                    metric: true,  // use metric or imperial units
+                                    onLocationError: function(err) {alert(err.message)},  // define an error callback function
+                                    onLocationOutsideMapBounds:  function(context) { // called when outside map boundaries
+                                        alert(context.options.strings.outsideMapBoundsMsg);
+                                    },
+                                    showPopup: true, // display a popup when the user click on the inner marker
+                                    strings: {
+                                        title: "Show me where I am",  // title of the locate control
+                                        metersUnit: "meters", // string for metric units
+                                        feetUnit: "feet", // string for imperial units
+                                        popup: "You are within {distance} {unit} from this point",  // text to appear if user clicks on circle
+                                        outsideMapBoundsMsg: "You seem located outside the boundaries of the map" // default message for onLocationOutsideMapBounds
+                                    },
+                                    locateOptions: {
+                                      enableHighAccuracy: true
+                                    }  // define location options e.g enableHighAccuracy: true or maxZoom: 10
+                                });
+
+                                lc.addTo(map);
+                                lc.start();
+
+
+            // Fonction pour changer les plans des étages selon le besoin.
+            function changerEtage(etage){
+
+                // Efface la calque qui contient l'image du plan de l'étage
+                map.removeLayer(planEtage);
+
+                if(etage == 1){
+
+                    // Efface le calque qui contient tout les marqueurs (portes, escalier, etc.) de l'étage 1
+                    map.removeLayer(marqueurs);
+
+                    imageUrl = 'images/etage1.svg';
+                    imageBounds = [[46.8291960, -71.229300],[46.8312300, -71.2251100]];
+
+                    planEtage = L.imageOverlay(imageUrl, imageBounds);
+
+                    // Ajoute et affiche l'image du plan de l'étage 1
+                    map.addLayer(planEtage);
+
+                    // Défini tous les marqueur sur l'étage 1
+                    porte1Icone = L.marker([46.8300549, -71.2277955], {icon: iconePortes}).bindPopup("Entrée principale");
+                    porte2Icone = L.marker([46.8297739, -71.2265661], {icon: iconePortes}).bindPopup("Entrée principale");
+                    porte3Icone = L.marker([46.8298144, -71.2264637], {icon: iconePortes}).bindPopup("Entrée principale");
+
+                    escalier1Icone = L.marker([46.8303292, -71.2271596], {icon: iconeEscalier}).bindPopup("Grand escalier");
+                    escalier2Icone = L.marker([46.8306716, -71.2274705], {icon: iconeEscalier}).bindPopup("Escalier 1E02");
+                    escalier3Icone = L.marker([46.8309851, -71.2277563], {icon: iconeEscalier}).bindPopup("Escalier 1E01");
+                    escalier5Icone = L.marker([46.8300009, -71.2268951], {icon: iconeEscalier}).bindPopup("Escalier 1E05");
+
+                    // Créé un "Layer Group" avec tout les marqueurs et les affiches sur la carte
+                    marqueurs = L.layerGroup([porte1Icone, porte2Icone, porte3Icone, escalier1Icone, escalier2Icone, escalier3Icone, escalier4Icone, escalier5Icone]);
+
+                    // Affiche la carte en arrière-plan de tout les autres calque. Nécésaire pour voir le point de géolocalisation après avoir changer d'étage
+                    planEtage.bringToBack();
+
+
+
+                }
+
+
+                // Change pour le 2e étage
+                else if (etage == 2){
+                    map.removeLayer(planEtage);
+                    map.removeLayer(marqueurs);
+                    imageUrl = 'images/etage2.svg';
+                    imageBounds = [[46.8291920, -71.2288570],[46.8312300, -71.2255300]];
+
+                    planEtage = L.imageOverlay(imageUrl, imageBounds)
+                    map.addLayer(planEtage);
+                    planEtage.bringToBack();
+                }
+
+
+                // Change pour le 3e étage
+                else if (etage == 3){
+                    map.removeLayer(planEtage);
+                    map.removeLayer(marqueurs);
+                    imageUrl = 'images/etage3.svg';
+                    imageBounds = [[46.8291920, -71.2288570],[46.8312300, -71.2255300]];
+
+                    planEtage = L.imageOverlay(imageUrl, imageBounds)
+                    map.addLayer(planEtage);
+                    planEtage.bringToBack();
+                }
+            } // Fin fonction changerEtage()
+
+
+
+            var marqueur = "";
+
+            // Ca contenir la postion du marqueur en object javascript dans le format ---> [lat, long]
+            var positionMarqueur = "";
+
+            // Fonction appellé chaque fois que l'on veux afficher un marqueur sur la carte pour montrer l'emplacement d'un local
+            function afficherMarqueur(position, etage, message){
+
+                // Suprime le calque "marqueur" qui contient le ou les marqueurs
+                map.removeLayer(marqueur);
+
+                // Transforme le string "postition" en objet pour être capable de l'afficher
+                positionMarqueur = JSON.parse(position);
+
+
+                map.panTo(new L.LatLng(46.8302871, -71.227337));
+
+                // Zoom la carte pour afficher tout le cégep
+                map.setZoom(19);
+
+                // Crée un marqueur et le fait rebondir selon les paramètres "duration" et "height"
+                marqueur = L.marker(positionMarqueur, {bounceOnAdd: true, bounceOnAddOptions: {duration: 500, height: 200}}).bindPopup(message)
+
+                // Créé un nouveau calque, ajoute le marqueur puis l'affiche sur la carte
+                map.addLayer(marqueur);
+
+                //Ouvre le popup du marqueur automatiquement (Contient la variable "message" de la fonction afficheMarqueur())
+                marqueur.openPopup();
+
+                // Ferme la fênetre modale
+                $('#modalRechercher').modal('hide')
+            };
+
+
+            // Document ready function
+            $(function(){
+
+                        // S'assure que la carte prend tout l'espace restant dans la fenêtre du navigateur
+                        var viewportHeight = $(window).height();
+                        var mapHeight = viewportHeight - $("footer").height();
+                        $("#map").height(mapHeight);
+
+                        // Cache tout les élément de la liste avec la classe "cacher"
+                        $(".cacher").hide();
+
+                        // Affiche le plan du 1er étage par defaut
+                        changerEtage(1);
+
+                        // Démare le service de géolcalisation
+                        lc.start();
+
+                        // Affiche les marqueurs du premier étage lors du chargemment du site
+                        porte1Icone = L.marker([46.8300549, -71.2277955], {icon: iconePortes}).bindPopup("Entrée principale");
+                        porte2Icone = L.marker([46.8297739, -71.2265661], {icon: iconePortes}).bindPopup("Entrée principale");
+                        porte3Icone = L.marker([46.8298144, -71.2264637], {icon: iconePortes}).bindPopup("Entrée principale");
+
+                        escalier1Icone = L.marker([46.8303292, -71.2271596], {icon: iconeEscalier}).bindPopup("Grand escalier");
+                        escalier2Icone = L.marker([46.8306716, -71.2274705], {icon: iconeEscalier}).bindPopup("Escalier 1E02");
+                        escalier3Icone = L.marker([46.8309851, -71.2277563], {icon: iconeEscalier}).bindPopup("Escalier 1E01");
+                        escalier4Icone = L.marker([46.8301899, -71.2277011], {icon: iconeEscalier}).bindPopup("Escalier 1E07");
+                        escalier5Icone = L.marker([46.8300009, -71.2268951], {icon: iconeEscalier}).bindPopup("Escalier 1E05");
+
+                        marqueurs = L.layerGroup([porte1Icone, porte2Icone, porte3Icone, escalier1Icone, escalier2Icone, escalier3Icone, escalier4Icone, escalier5Icone]);
+
+                        // Affiche seulement les marqueurs des portes si la carte est asser zoomer (Level 19)
+                        map.on('zoomend', function() {
+                        if (map.getZoom() > 19) {
+                            map.addLayer(marqueurs);
+                        } else if (map.getZoom() < 20){
+                            map.removeLayer(marqueurs);
+                        }
+                        });
+
+
+
+                        // Ouvre la fenêtre modale avec le bouton dans le menu de navigation
+                        $("#boutonRechercher").click(function(event){
+                            event.preventDefault();
+                            $("#modalRechercher").modal('show')
+                            $('#modalRechercher').on('shown.bs.modal', function () {
+                                $('#inputRechercherModal').focus()
+                            })
+                        });
+
+
+                        // Recherche d'un local
+                        // Filtre la liste dynamiquement selon la vauleur du champ de texte
+                        $('#listeRechercheLocal').liveFilter('#inputRechercherModal', 'li', {
+                            filterChildSelector: 'a',
+                        });
+
+
+            }); // Document ready function
